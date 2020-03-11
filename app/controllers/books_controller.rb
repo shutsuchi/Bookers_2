9802e632282f702d81b23aa0b1877905f1308655
shutsuchi <wwww.www.wshu@gmail.com>
2020-03-11 19:33:16 +0900

    [finish] fav機能非同期化(Ajax)完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_comment = BookComment.new
+    #@book_comment = BookComment.new
+    @the_book_comment = BookComment.new
+    @book_comment = BookComment.find(params[:id])
     @book_comments = @book.book_comments.all
     @thebook = Book.new
     @user = User.new
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     respond_to do |format|
       if favorite.save
-        format.html
+        #format.html
         format.js
       end
     end
@@ -13,15 +13,14 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     respond_to do |format|
       if favorite.destroy
-        format.html
+        #format.html
         format.js
       end
     end
-    #favorite.destroy
-    #  redirect_to book_path(book)
+    #redirect_to book_path(book)
   end
 end
